4ffae0574998f4e2beabce98fceb3cfb57819c07
yentin3 <yentin3@gmail.com>
2014-07-08 12:13:45 -0700

    new for slave branch

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -2,5 +2,6 @@
 public class HelloGitHub {
     public static void main(String[] args) {
         System.out.println("Hello GitHub Master!");
+        System.out.println("Hello GitHub Slave!");
     }
 }
