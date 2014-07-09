2c42f0e0107a5231a4add5d6af093e13c41ef856
yentin3 <yentin3@gmail.com>
2014-07-08 23:36:07 -0700

    Master +test

--- src/HelloGitHub.java
+++ src/HelloGitHub.java
@@ -1,6 +1,6 @@
 
 public class HelloGitHub {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub Master!");
+        System.out.println("Hello GitHub Master + TEST!");
     }
 }
